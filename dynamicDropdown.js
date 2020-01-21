b832de72db5f675ea0f75ec51d4f1fcdd3dbe13a
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-01-21 22:16:40 +0500

    dynamicDropdown implemented

---
+++ dynamicDropdown.js
@@ -0,0 +1,8 @@
+ let startYear = new Date().getFullYear()
+    for (y = startYear; y >= 2017; y--) {
+        var optn = document.createElement("OPTION");
+        optn.text = y;
+        optn.value = y;
+        document.getElementById('year').options.add(optn);
+        document.getElementById('year').style.color = 'black';
+    }
